7ec78824879a2409c0b8222539f33baf3c5ad51b
Jorge Crespo <jorge_crespo_c@hotmail.com>
2018-07-08 17:08:22 -0500

    Remove code from controller.m from public

--- src/controller.m
+++ src/controller.m
@@ -18,70 +18,8 @@ function [F, M] = controller(t, state, des_state, params)
 
 % =================== Your code goes here ===================
 
-% Thrust
-F = 0;
+% This code is not public. If you would like to see it, please let me know.
 
-% Moment
-M = zeros(3,1);
-
-% error in position
-ep = des_state.pos - state.pos;
-
-% error in velocity
-ev = des_state.vel - state.vel;
-
-% K's
-Kpx = 20;
-Kdx = 1;
-Kpy = 20;
-Kdy = 1;
-Kpz = 15;
-Kdz = 8;
-
-Kpphi = 5;
-Kdphi = 0.45;
-Kptheta = 5;
-Kdtheta = 0.45;
-Kppsi = 5;
-Kdpsi = 0.45;
-
-Kp_angles = [Kpphi Kptheta Kppsi];
-Kd_angles = [Kdphi Kdtheta Kdpsi];
-
-% commanded accedelations
-x_c_ddot = des_state.acc(1) + Kpx*ep(1) + Kdx*ev(1);
-y_c_ddot = des_state.acc(2) + Kpy*ep(2) + Kdy*ev(2);
-z_c_ddot = des_state.acc(3) + Kpz*ep(3) + Kdz*ev(3);
-
-% u1
-u1 = params.mass*(params.gravity + z_c_ddot);
-
-% angles_c
-phi_c = (x_c_ddot*sin(des_state.yaw) - y_c_ddot*cos(des_state.yaw))/params.gravity;
-theta_c = (x_c_ddot*cos(des_state.yaw) + y_c_ddot*sin(des_state.yaw))/params.gravity;
-angles_c = [phi_c; theta_c; des_state.yaw];
-
-% x_c_dddot
-x_c_dddot = Kpx*ev(1) + Kdx*(des_state.acc(1) - x_c_ddot);
-
-% y_c_dddot
-y_c_dddot = Kpy*ev(2) + Kdy*(des_state.acc(2) - y_c_ddot);
-
-% angles_dot_c
-phi_dot_c = ((x_c_ddot*cos(des_state.yaw) + y_c_ddot*sin(des_state.yaw))*des_state.yawdot + x_c_dddot*sin(des_state.yaw) - y_c_dddot*cos(des_state.yaw))/params.gravity;
-theta_dot_c = ((y_c_ddot*cos(des_state.yaw) - x_c_ddot*sin(des_state.yaw))*des_state.yawdot + x_c_dddot*cos(des_state.yaw) + y_c_dddot*sin(des_state.yaw))/params.gravity;
-angles_dot_c = [phi_dot_c; theta_dot_c; des_state.yawdot];
-
-% from angle_dot to angular velocities
-R_a2w=[cos(theta_c) 0 -cos(phi_c)*sin(theta_c); 0 1 sin(phi_c); sin(theta_c) 0 cos(phi_c)*cos(theta_c)];
-w_c = R_a2w*angles_dot_c;
-
-u2 = diag(Kp_angles)*(angles_c - state.rot) + diag(Kd_angles)*(w_c - state.omega);
-
-% disp(u1);
-% disp(u2);
-F = u1;
-M = u2;
 % =================== Your code ends here ===================
 
 end
